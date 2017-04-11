c6e3376edd1765cad9cb8052d2bd7472d213e501
wss534857356 <534857356@qq.com>
2017-04-11 21:31:04 +0800

    add SideBar

---
+++ client/components/MusicBoard/index.js
@@ -0,0 +1,14 @@
+
+import react, { Component } from 'react'
+import style from './style.scss'
+import SideBar from '../SideBar'
+
+class MusicBoard extends Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default MusicBoard
---
+++ client/components/SideBar/index.js
@@ -0,0 +1,41 @@
+
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+import { SEARCH_LIST_MENU, PLAY_LIST_MENU} from '../../constants/filters'
+import style from './style.scss'
+
+const MENU_FILTERS = {
+  [SEARCH_LIST_MENU]: '搜索',
+  [PLAY_LIST_MENU]: '播放列表'
+}
+class SideBar extends Component {
+  renderMenuLink(filter) {
+    const title = filter
+    return (
+      <Link
+        activeClassName={style.button_active}
+        className={style.button}
+        to={filter}
+        style={{ cursor: 'pointer' }}>
+        <div>
+          { MENU_FILTERS[title] }
+        </div>
+      </Link>
+    )
+  }
+  render() {
+    return (
+      <div className={style.normal}>
+        <ul className={style.menus}>
+          {[SEARCH_LIST_MENU, PLAY_LIST_MENU].map(filter =>
+            <li key={filter}>
+              {this.renderMenuLink(filter)}
+            </li>
+          )}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default SideBar
